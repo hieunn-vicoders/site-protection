b3d80307f18fd18cf3e34632fb7734e063d293af
Nguyen Nhat Linh <linhnn@vmms.vn>
2020-12-14 16:50:01 +0700

    Fix: SiteProtectionMiddleWare.php add hash::check

--- src/app/Http/Middlewares/SiteProtectionMiddleWare.php
+++ src/app/Http/Middlewares/SiteProtectionMiddleWare.php
@@ -3,13 +3,13 @@
 namespace VCComponent\Laravel\Site\Http\Middlewares;
 
 use Closure;
-
+use Illuminate\Support\Facades\Hash;
 class SiteProtectionMiddleWare
 {
     public function handle($request, Closure $next)
     {
         if (config('site-protection.enable') === true) {
-            if (isset($_COOKIE['site-authentication'])) {
+            if (isset($_COOKIE['site-authentication']) && Hash::check(config('site-protection.custom_or_default.account'), $_COOKIE['site-authentication'])) {
                 return $next($request);
             }
             $current_route = $request->path();
