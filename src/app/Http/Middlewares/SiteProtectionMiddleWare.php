ebbbc8056c3f8cb8dca958d6a1e772725c9f70e0
Nguyen Nhat Linh <linhnn@vmms.vn>
2020-12-31 16:20:36 +0700

    Fix: SiteProtectionMiddleWare.php checkQueryString

--- src/app/Http/Middlewares/SiteProtectionMiddleWare.php
+++ src/app/Http/Middlewares/SiteProtectionMiddleWare.php
@@ -12,21 +12,36 @@ class SiteProtectionMiddleWare
 
     public function __construct(Request $request)
     {
-        $this->route = $request->path();
+        $this->route     = $request->path();
     }
 
     public function handle($request, Closure $next)
     {
-        if (config('site-protection.enable') === true) {
-            if (isset($_COOKIE['site-authentication']) && Hash::check(config('site-protection.custom_or_default.password'), $_COOKIE['site-authentication'])) {
-                return $next($request);
+        if (!$this->checkRequest($request)) {
+            if (config('site-protection.enable') === true) {
+                if (isset($_COOKIE['site-authentication']) && Hash::check(config('site-protection.custom_or_default.password'), $_COOKIE['site-authentication'])) {
+                    return $next($request);
+                }
+                if ($this->route == "/") {
+                    return redirect()->route('show.protection', ['route' => "home"]);
+                } else {
+                    return redirect()->route('show.protection', ['route' => $this->route]);
+                }
             }
-            if ($this->route == "/") {
-                return redirect()->route('show.protection', ['route' => "home"]);
-            } else {
-                return redirect()->route('show.protection', ['route' => $this->route]);
+        } else {
+            return $next($request);
+        }
+    }
+
+    public function checkRequest(Request $request)
+    {
+        if ($request->has('site_protection_username') && $request->has('site_protection_password')) {
+            $site_protection_username = config('site-protection.custom_or_default.account');
+            $site_protection_password = config('site-protection.custom_or_default.password');
+            if ($request->get('site_protection_username') === $site_protection_username && $request->get('site_protection_password') === $site_protection_password) {
+                return true;
             }
         }
-        return $next($request);
+        return false;
     }
 }
