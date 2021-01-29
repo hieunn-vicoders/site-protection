ee6a99b2e50e547a010da9180f18783730ba3c29
Nguyen Nhat Linh <linhnn@vmms.vn>
2021-01-29 16:52:20 +0700

    Fix: Redirect 404 page if not isset config/site-protection.php

--- src/routes/route.php
+++ src/routes/route.php
@@ -6,10 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::post('/check-form-login-site-protection', 'VCComponent\Laravel\Site\Http\Controllers\SiteProtectionController@checkFormLoginSiteProtection')->name("site.protection");
 Route::get('/login-internal', 'VCComponent\Laravel\Site\Http\Controllers\SiteProtectionController@showFormLoginSiteProtection')->name('show.protection');
 Route::fallback(function (Request $request) {
-    if (isset($_COOKIE['site-authentication']) || config('site-protection.enable') === false) {
+    if (isset($_COOKIE['site-authentication']) || config('site-protection.enable') === false || (!config('site-protection'))) {
         return abort(404);
     }
     $route = $request->path();
     return redirect()->route('show.protection', ['route' => $route]);
 });
-
