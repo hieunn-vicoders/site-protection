4c7f1aceeb42909af07e9ce02a66225cc9956454
Nguyen Nhat Linh <linhnn@vmms.vn>
2021-02-23 09:47:19 +0700

    Fix: Route fallback

--- src/routes/route.php
+++ src/routes/route.php
@@ -6,3 +6,13 @@ use Illuminate\Support\Facades\Route;
 Route::post('/check-form-login-site-protection', 'VCComponent\Laravel\Site\Http\Controllers\SiteProtectionController@checkFormLoginSiteProtection')->name("site.protection");
 Route::get('/login-internal', 'VCComponent\Laravel\Site\Http\Controllers\SiteProtectionController@showFormLoginSiteProtection')->name('show.protection');
 
+Route::fallback(function (Request $request) {
+    if (config('site-protection.enable') === true) {
+        if (isset($_COOKIE['site-authentication'])) {
+            return abort(404);
+        }
+        $route = $request->path();
+        return redirect()->route('show.protection', ['route' => $route]);
+    }
+    return abort(404);
+});
